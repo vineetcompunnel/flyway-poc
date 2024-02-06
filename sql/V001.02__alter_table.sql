28d68a553a03dc1808ffeb46b5293665b3ff0cd4
vineetcompunnel <144663532+vineetcompunnel@users.noreply.github.com>
2024-02-06 17:36:49 +0530

    Delete sql/V001.02__alter_table.sql

--- sql/V001.02__alter_table.sql
+++
@@ -1,8 +0,0 @@
--- Adding a new column 'PhoneNumber' to the 'SampleTable'
-ALTER TABLE SampleTable
-ADD PhoneNumber NVARCHAR(20);
-
-
--- V2__add_column.sql (Fixed version)
-ALTER TABLE SampleTable
-ADD new_column INT;
